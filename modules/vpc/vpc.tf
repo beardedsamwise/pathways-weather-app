56f39d5f440afff888c6ab9865754a02109d1a20
Sam Bentley <sam.bentley@contino.io>
2022-02-21 15:07:47 +1100

    add ngws and public route table

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -78,18 +78,69 @@ resource "aws_eip" "ngw" {
   vpc      = true
   tags = {
     Name = "${var.prefix}_eip"
-  }
+}
 }
 
-# create NAT gateways
-# resource "aws_nat_gateway" "ngw" {
-#   for_each = aws_subnet.public
-#   allocation_id = aws_eip.ngw[count.index]
-#   subnet_id = aws_subnet.public[count.index]
-#   tags = {
-#     Name = "${var.prefix}_eip"
-#   } 
-#   depends_on = [aws_internet_gateway.igw, aws_eip.ngw]
+# data "aws_subnet" "public" {
+#   for_each = data.aws_subnet.public
+#   id = each.value.id
 # }
 
+# data "aws_eip" "public" {
+#   for_each = aws_eip.ngw
+#   id = each.value.id
+# }
+
+# create NAT gateways
+resource "aws_nat_gateway" "ngw" {
+  count = length(aws_subnet.public)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id = aws_subnet.public[count.index].id
+  tags = {
+    Name = "${var.prefix}_eip"
+  } 
+  depends_on = [aws_internet_gateway.igw, aws_eip.ngw]
+}
+
+# create route tables
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.prefix}_public_rtb"
+  }
+  depends_on = [aws_internet_gateway.igw]
+}
+
+# resource "aws_route_table" "private" {
+#   count = length(aws_nat_gateway)
+#   vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.ngw[count.index].id
+#   }
+
+#   tags = {
+#     Name = "${var.prefix}_public_rtb"
+#   }
+#   depends_on = [aws_nat_gateway.ngw]
+# }
+
+# assosciate route tables with subnets
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# resource "aws_route_table_association" "private" {
+#   for_each       = aws_subnet.private
+#   subnet_id      = each.value.id
+#   route_table_id = aws_route_table.private.id
+# }
 
