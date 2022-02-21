b4d637caf58fe35355e3015ecf711d16d4c9dc1f
Sam Bentley <sam.bentley@contino.io>
2022-02-21 15:10:35 +1100

    minor change to force actions run

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -81,16 +81,6 @@ resource "aws_eip" "ngw" {
 }
 }
 
-# data "aws_subnet" "public" {
-#   for_each = data.aws_subnet.public
-#   id = each.value.id
-# }
-
-# data "aws_eip" "public" {
-#   for_each = aws_eip.ngw
-#   id = each.value.id
-# }
-
 # create NAT gateways
 resource "aws_nat_gateway" "ngw" {
   count = length(aws_subnet.public)
@@ -138,9 +128,4 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-# resource "aws_route_table_association" "private" {
-#   for_each       = aws_subnet.private
-#   subnet_id      = each.value.id
-#   route_table_id = aws_route_table.private.id
-# }
 
