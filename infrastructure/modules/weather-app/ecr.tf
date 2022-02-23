3b51a44f4346b1a81e2046fd45e8fd9c912951c9
Sam Bentley <sam.bentley@contino.io>
2022-02-24 10:24:27 +1100

    change image_tag_mutability to MUTABLE

--- infrastructure/modules/weather-app/ecr.tf
+++ infrastructure/modules/weather-app/ecr.tf
@@ -1,7 +1,7 @@
 # deploy ECR instance
 resource "aws_ecr_repository" "ecr" {
   name                 = "${var.git_username}-node-weather-app"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
