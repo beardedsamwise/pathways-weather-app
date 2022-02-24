70e99173ed5d3f81bd8eae39d9fbabaa844e70ec
Sam Bentley <sam.bentley@contino.io>
2022-02-24 14:23:32 +1100

    change s3_bucket_name output to .bucket

--- infrastructure/modules/s3/s3.tf
+++ infrastructure/modules/s3/s3.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "this" {
 ### Define Output
 output "s3_bucket_name" {
   description = "The name of the bucket"
-  value       = aws_s3_bucket.this.id
+  value       = aws_s3_bucket.this.bucket
 }
 
 output "s3_bucket_name_arn" {
