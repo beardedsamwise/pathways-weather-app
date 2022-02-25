f025f46fbddc4d1972c626a9327da243cc46c838
Sam Bentley <sam.bentley@contino.io>
2022-02-25 11:47:45 +1100

    add ecs service for weather-app

--- infrastructure/modules/weather-app/ecs.tf
+++ infrastructure/modules/weather-app/ecs.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_cluster" "weather-app" {
 }
 
 # create task definition
-resource "aws_ecs_task_definition" "service" {
+resource "aws_ecs_task_definition" "weather-app" {
   family = "${var.prefix}-weather-app-fam"
   network_mode = "awsvpc"
   execution_role_arn = aws_iam_role.ecs.arn
@@ -35,18 +35,25 @@ resource "aws_ecs_task_definition" "service" {
   )
 }
 
-# create ECS service
-# resource "aws_ecs_service" "weather-app" {
-#   name            = "weather-app-service"
-#   cluster         = aws_ecs_cluster.weather-app.id
-#   #task_definition = aws_ecs_task_definition.mongo.arn
-#   desired_count   = 3
-#   iam_role        = aws_iam_role.ecs.arn
-#   depends_on      = [aws_iam_policy.ecs]
+# create ECS service (deploy weather-app container)
+resource "aws_ecs_service" "weather-app" {
+  name            = "weather-app"
+  cluster         = aws_ecs_cluster.weather-app.id
+  task_definition = aws_ecs_task_definition.weather-app.arn
+  desired_count   = 1
+  iam_role        = aws_iam_role.ecs.arn
+  depends_on      = [aws_iam_role.ecs, aws_iam_policy.ecs, aws_ecs_task_definition.weather-app]
 
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.weather-app.arn
-#     container_name   = "weather-app"
-#     container_port   = 3000
-#   }
-# }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.weather-app.arn
+    container_name   = "weather-app"
+    container_port   = 3000
+  }
+
+    network_configuration {
+    security_groups  = [aws_security_group.ecs.id]
+    subnets          = [var.private_subnet_ids[0], var.private_subnet_ids[1]]
+    assign_public_ip = false
+  }
+}
+
